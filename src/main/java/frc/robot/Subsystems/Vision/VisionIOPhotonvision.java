8a27af49e80ebbdbf21db1b959e321df1cbaf8c4
holyb3anS <144182597+holyb3anS@users.noreply.github.com>
2025-10-20 02:08:12 -0600

    testing git build

--- src/main/java/frc/robot/Subsystems/Vision/VisionIOPhotonvision.java
+++ src/main/java/frc/robot/Subsystems/Vision/VisionIOPhotonvision.java
@@ -20,7 +20,7 @@ public class VisionIOPhotonvision implements VisionIO {
    * Creates a new VisionIOPhotonVision.
    *
    * @param name The configured name of the camera.
-   * @param rotationSupplier The 3D position of the camera relative to the robot.
+   * @param robotToCamera The 3D position of the camera relative to the robot.
    */
   public VisionIOPhotonvision(String name, Transform3d robotToCamera) {
     camera = new PhotonCamera(name);
